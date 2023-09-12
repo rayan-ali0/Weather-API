0403d6a599caad4f5318ccb4303575519c281cfd
Rayan ALi <rayan.ali.8205@gmail.com>
2023-09-12 20:19:13 +0300

    added my name to the app

--- src/App.js
+++ src/App.js
@@ -21,9 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
-        <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+<h1> Hello , This is just for trying</h1>
       </div>
     );
   }
